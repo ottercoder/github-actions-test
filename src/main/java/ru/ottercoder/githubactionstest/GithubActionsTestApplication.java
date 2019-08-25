b95db23e5901a12add5777b5c95cb2a00907a892
Kirill <ottercoder@gmail.com>
2019-08-25 15:01:56 +0300

    [NF] Adding basic functionality

---
+++ src/main/java/ru/ottercoder/githubactionstest/GithubActionsTestApplication.java
@@ -0,0 +1,13 @@
+package ru.ottercoder.githubactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsTestApplication.class, args);
+	}
+
+}
